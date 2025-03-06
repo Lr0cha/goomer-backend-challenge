a3a5e995148bc61a7b0c32a4d03e41c1dfe993cc
Lucas Rocha <ldrocha16@gmail.com>
2025-03-06 11:10:34 -0300

    chore: initial project setup

---
+++ src/main/java/com/api/goomer/GoomerApplication.java
@@ -0,0 +1,13 @@
+package com.api.goomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/goomer/GoomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.goomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
